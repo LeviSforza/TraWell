0062000ffd95e75a73269c56579c734914e668d5
Monika Galińska <3monika100@gmail.com>
2022-11-13 16:26:52 +0100

    Add some steps

--- TraWell-tests/features/environment.py
+++ TraWell-tests/features/environment.py
@@ -6,12 +6,19 @@ from seleniumrequests import RequestMixin # or your own mixin
 class BehaveRequestDriver(BehaveDriverMixin, RequestMixin, webdriver.Chrome):
     pass
 
-def before_all(context):
-    context.behave_driver = behave_webdriver.Chrome()
+# def before_all(context):
+#     context.behave_driver = behave_webdriver.Chrome()
 
-def after_all(context):
-    # cleanup after tests run
-    context.behave_driver.quit()
+# def after_all(context):
+#     # cleanup after tests run
+#     context.behave_driver.quit()
 
+def before_feature(context, feature):
+    context.driver = webdriver.Chrome()
+    context.driver.implicitly_wait(2)
+
+    
+def after_feature(context, feature):
+    context.driver.quit()
 
 # https://testuj.pl/blog/test-automatyczny-bdd-w-selenium-z-wykorzystaniem-python-i-behave-instrukcja-krok-po-kroku/
--- TraWell-tests/features/steps/login.py
+++ TraWell-tests/features/steps/login.py
@@ -2,23 +2,25 @@ from behave import *
 
 
 @given("I am on the login page")
-def step_impl():
-    raise NotImplementedError(u'STEP: Given I am on the login page')
+def i_am_on_the_login_page(context):
+    context.driver.get('http://localhost:5674/home')
+    context.driver.find_element_by_css_selector('input.loginButton').click()
 
 
 @given("the field 'Email' is empty")
-def step_impl():
-    raise NotImplementedError(u'STEP: And the field \'Email\' is empty')
+def the_field_email_is_empty(context):
+    email_field = context.driver.find_element_by_id('f_login')
+    assert email_field.is_empty()
 
 
 @given("the field 'Password' is empty")
-def step_impl():
+def the_field_password_is_empty(context):
     raise NotImplementedError(u'STEP: And the field \'Password\' is empty')
 
 
 @when("I click on 'SIGN IN'")
-def step_impl():
-    raise NotImplementedError(u'STEP: When I click on \'SIGN IN\'')
+def i_click_on_sign_in(context):
+    context.driver.find_element_by_css_selector('input.loginButton').click()
 
 
 @then("I should see prompt 'Invalid username or password.'")
@@ -26,31 +28,28 @@ def step_impl():
     raise NotImplementedError(u'STEP: Then I should see prompt \'Invalid username or password.\'')
 
 
-@when("I type 'olga@tokarczuk.com' in 'Email'")
-def step_impl():
-    raise NotImplementedError(u'STEP: When I type \'olga@tokarczuk.com\' in \'Email\'')
+@when("I type '{email}' in 'Email'")
+def step_impl(context, email):
+    context.driver.find_element_by_id('f_login').send_keys(email)
 
 
-@given("I type 'wrong_pass' in 'Password'")
-def step_impl():
-    raise NotImplementedError(u'STEP: And I type \'wrong_pass\' in \'Password\'')
+@given("I type '{password}' in 'Password'")
+def step_impl(context, password):
+    context.driver.find_element_by_id('f_password').send_keys(password)
 
 
 @given("I have users:")
-def step_impl():
+def step_impl(context):
+    for row in context.table:
+        context.model.add_user(row["name"], deparment=row["department"])
     raise NotImplementedError(u'STEP: Given I have users')
 
 
-@given("I type 'correct_pass' in 'Password'")
-def step_impl():
-    raise NotImplementedError(u'STEP: And I type \'correct_pass\' in \'Password\'')
-
-
 @then("I should be on the users home page")
-def step_impl():
+def i_should_be_on_the_users_home_page(context):
     raise NotImplementedError(u'STEP: Then I should be on the users home page')
 
 
 @given("I shouldn't see 'LOGIN' button")
-def step_impl():
+def i_shouldnt_see_login_button(context):
     raise NotImplementedError(u'STEP: And I shouldn\'t see \'LOGIN\' button')
